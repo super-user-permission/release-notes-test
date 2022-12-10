99ffc7e61ff58f2b4d7a703007e48aec298e2ed9
Somesh Yadav <somesh19.1996@gmail.com>
2022-12-10 19:21:29 +0530

    feat(FSD-5601): New component added

--- src/src/index.js
+++ src/src/index.js
@@ -1,6 +1,12 @@
 import { render } from '@testing-library/react';
-import React from 'react';
+import React, { useState } from 'react';
 
 function ReactNewComponent() {
+    const [data, setData] = useState('');
 
+    return (
+        <div>
+            <span>New data fooled rooted googled</span>
+        </div>
+    )
 }
